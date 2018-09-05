ce067d52d1b9340c16b2e77eb57235453c7dc2db
Anton Katliar <Anton_Katliar@epam.com>
2018-09-05 12:26:45 +0300

    period length testing

--- prng.js
+++ prng.js
@@ -13,32 +13,54 @@ class PRNG {
         // Verify that seed is an integer
         if (seed % 1 === 0) {
             // Initialize seed with a modulo by n
-            this.seed = seed % 2147483647
+            this.seed = seed % 2147483647;
             if (this.seed <= 0) {
                 // If seed is negative or zero, add n
-                this.seed += 2147483646
+                this.seed += 2147483646;
             }
         } else {
-            throw new Error('Seed value must be an integer.')
+            throw new Error('Seed value must be an integer.');
         }
     }
 
     /** Return a pseudo-random value between 1 and n */
     next () {
         // x_k+1 = (g * x_k) % n
-        return this.seed = this.seed * 16807 % 2147483647
+        return this.seed = this.seed * 16807 % 2147483647;
     }
 
     /** Return a pseudo-random floating point number in range [0, 1] */
     nextFloat () {
         // We know that result of next() will be 1 to n (inclusive)
-        return (this.next() - 1) / 2147483646
+        return (this.next() - 1) / 2147483646;
     }
 }
 
 const seed = Math.floor(Math.random() * (10 ** 12));
 const randomNumberGenerator = new PRNG(seed);
-const generateAmountOfValues = amountV => {
-    const arrayOfRandomNumbers = new Array(amountV);
-    return arrayOfRandomNumbers.fill(0).map(() => randomNumberGenerator.nextFloat());
+const AMOUNT_OF_RANDOM_NUMBERS = 50000;
+const generateAmountOfValues = (
+    amountV = 1,
+    randomGenerator = {
+        nextFloat: () => Date.now(),
+    },
+) => {
+    const mockArrayOfRandomNumbers = new Array(amountV);
+    return mockArrayOfRandomNumbers.fill(0).map(() => randomGenerator.nextFloat());
 }
+
+const getPeriodLength = (Xv = 1, arrayOfRandomNumbers = []) => {
+    //TODO: SLICE ARRAY AFTER VALUE WAS FOUND
+    const i2 = arrayOfRandomNumbers.findIndex(randomNumber => randomNumber === Xv);
+    const i1 = arrayOfRandomNumbers.findIndex(randomNumber => randomNumber === Xv);
+    return (i2 - i1) || arrayOfRandomNumbers.length;
+}
+
+const arrayOfRandomNumbers = generateAmountOfValues(AMOUNT_OF_RANDOM_NUMBERS, randomNumberGenerator);
+const Xv = arrayOfRandomNumbers[arrayOfRandomNumbers.length - 1];
+const periodLength = getPeriodLength(
+    Xv,
+    generateAmountOfValues(AMOUNT_OF_RANDOM_NUMBERS, randomNumberGenerator),
+);
+
+console.log(periodLength);
