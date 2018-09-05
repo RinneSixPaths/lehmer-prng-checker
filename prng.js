f5be6cb83218fdf5718df60080e0ef17edd86ea0
RinneSixPaths <katlyar.anton@mail.ru>
2018-09-05 17:29:35 +0300

    implement aperiodic length

--- prng.js
+++ prng.js
@@ -1,24 +1,11 @@
 class PRNG {
-    /**
-     * Create a pseudo-random number generator. The seed must be an integer.
-     *
-     * Uses the Lehmer / Park-Miller PRNG
-     * https://en.wikipedia.org/wiki/Lehmer_random_number_generator
-     *
-     *  Utilizes MINSTD parameters where:
-     *  n = 2^31 − 1 = 2,147,483,647 (a Mersenne prime)
-     *  g = 7^5 = 16,807 (a primitive root modulo)
-     */
     constructor (seed) {
         
         this.m = Math.floor(Math.random() * (10 ** 101));
         this.a = Math.floor(Math.random() * (10 ** 90));
-        // Verify that seed is an integer
         if (seed % 1 === 0) {
-            // Initialize seed with a modulo by n
             this.seed = seed % this.m;
             if (this.seed <= 0) {
-                // If seed is negative or zero, add n
                 this.seed += this.m;
             }
         } else {
@@ -26,22 +13,18 @@ class PRNG {
         }
     }
 
-    /** Return a pseudo-random value between 1 and n */
     next () {
-        // x_k+1 = (g * x_k) % n
         return this.seed = this.seed * this.a % this.m;
     }
 
-    /** Return a pseudo-random floating point number in range [0, 1] */
     nextFloat () {
-        // We know that result of next() will be 1 to n (inclusive)
         return (this.next() - 1) / this.m;
     }
 }
 
-const seed = Math.floor(Math.random() * (10 ** 100));//Math.floor(Math.random() * (10 ** 12));
+const seed = Math.floor(Math.random() * (10 ** 100));
 const randomNumberGenerator = new PRNG(seed);
-const AMOUNT_OF_RANDOM_NUMBERS = 5000000;
+const AMOUNT_OF_RANDOM_NUMBERS = 10000000;
 const generateAmountOfValues = (
     amountV = 1,
     randomGenerator = {
@@ -53,7 +36,6 @@ const generateAmountOfValues = (
 }
 
 const getPeriodLength = (Xv = 1, arrayOfRandomNumbers = []) => {
-    //TODO: SLICE ARRAY AFTER VALUE WAS FOUND
     const i2 = arrayOfRandomNumbers.findIndex(randomNumber => randomNumber == Xv);
     arrayOfRandomNumbers.splice((arrayOfRandomNumbers.length - 1), 1);
     const i1 = arrayOfRandomNumbers.findIndex(randomNumber => randomNumber == Xv);
@@ -71,18 +53,29 @@ const periodLength = getPeriodLength(
 
 console.log(periodLength);
 
-/*const pairs = arrayOfRandomNumbers.map((value, index, arr) => {
-    if ((index + 1)%2 === 0) {
-        return [arr[index], value];
+const checkByIndirectEvidence = (amounOfNumbers = 1) => {
+    const pairs = [];
+    for (let index = 0; index < amounOfNumbers; index++) {
+        if ((index + 1)%2 === 0) {
+            pairs.push([arrayOfRandomNumbers[index - 1], arrayOfRandomNumbers[index]]);
+        }
     }
-});*/
-const pairs = [];
-for (let index = 0; index < AMOUNT_OF_RANDOM_NUMBERS; index++) {
-    if ((index + 1)%2 === 0) {
-        pairs.push([arrayOfRandomNumbers[index - 1], arrayOfRandomNumbers[index]]);
-    }
+
+    const filtered = pairs.filter((twoValArray) => (
+        twoValArray[0]**2 + twoValArray[1]**2 < 1
+    ));
+    const isPlaneNumbers = 2 * filtered.length / amounOfNumbers;
+    console.log(isPlaneNumbers);
+    console.log(Math.PI / 4);
+    return isPlaneNumbers;
 }
 
-const filtered = pairs.filter((twoValArray) => (
-    twoValArray[0]**2 + twoValArray[1]**2 < 1
-));
+checkByIndirectEvidence(AMOUNT_OF_RANDOM_NUMBERS);
+
+const getAperiodicLength = (arrayOfRandomNumbers = [], periodLength = 1) => {
+    const minimalIndex = arrayOfRandomNumbers.findIndex(randomValue => randomValue === randomValue + periodLength);
+    return (minimalIndex + 1 + periodLength);
+}
+
+const aperiodicLength = getAperiodicLength(arrayOfRandomNumbers, periodLength);
+console.log(aperiodicLength);
